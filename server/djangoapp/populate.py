954b5533ac48645e84bab634fb4ef6237399b7e2
Sai Prashanth <171444765+KSaiPrashanth@users.noreply.github.com>
2024-09-18 17:15:31 +0530

    fixing lint errors in populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -10,17 +10,17 @@ def initiate():
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
-    car_make_instances = []
+   car_make_instances = []
     for data in car_make_data:
-            car_make_instances.append(
-                CarMake.objects.create(
-                    name=data['name'],
-                    description=data['description']
-                )
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data["name"],
+                description=data["description"]
             )
+        )
 
 
-    # Create CarModel instances with the corresponding CarMake instances
+# Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {
           "name": "Pathfinder",
@@ -115,9 +115,9 @@ def initiate():
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(
-                name=data['name'],
-                car_make=data['car_make'],
-                type=data['type'],
-                year=data['year']
-            )
+        CarModel.objects.create(
+            name=data["name"],
+            car_make=data["car_make"],
+            type=data["type"],
+            year=data["year"],
+        )
