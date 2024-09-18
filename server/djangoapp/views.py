731a3c080503d16cd16a5c4b625d0629e7aa487a
Sai Prashanth <171444765+KSaiPrashanth@users.noreply.github.com>
2024-09-18 18:45:32 +0530

    fixing lint errors in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,11 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
+# from django.contrib import messages
 # from datetime import datetime
 from .models import CarMake, CarModel
 from django.http import JsonResponse
@@ -14,7 +14,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments, post_review, searchcars_request
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
