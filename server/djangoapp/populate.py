57b28e5aa3f6d5683506aaeda1efb5f0e1994aba
Sai Prashanth <171444765+KSaiPrashanth@users.noreply.github.com>
2024-09-18 17:22:04 +0530

    fixing lint errors in populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -9,7 +9,7 @@ def initiate():
         {"name": "Kia", "description": "Great cars. Korean technology"},
         {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
-    
+
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
